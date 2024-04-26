23fc90c6be032d32e9980b9b243c3f6628c45745
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-04-26 15:26:35 +0700

    first commit

---
+++ src/test/java/i4b_workflows/AppTest.java
@@ -0,0 +1,20 @@
+package i4b_workflows;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
